f7b3a8776530e80d0b2152c856a8efad24be7550
Razvan Ilin <razvanilin@gmail.com>
2019-12-18 21:50:46 +0000

    :wrench: The env vars are now fetched from the root folder

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const express = require("express");
 const methodOverride = require("method-override");
--- server/models/config/config.js
+++ server/models/config/config.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "..", "..", "..", ".env") });
 
 module.exports = {
   development: {
