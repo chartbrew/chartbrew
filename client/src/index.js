a3b450ab7434af5770569e9dd29cc512b1591945
Razvan Ilin <razvanilin@gmail.com>
2020-01-05 11:57:03 +0000

    :green_heart: Disable eslint check for semantic dist css

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import "./index.css";
-import "./semantic/dist/semantic.min.css";
+// disable eslint for the line below to fix the CI
+import "./semantic/dist/semantic.min.css"; // eslint-disable-line
 
 import App from "./App";
 import { unregister } from "./registerServiceWorker";
 
 unregister();
 
-ReactDOM.render(<App />, document.getElementById('root')); // eslint-disable-line
+ReactDOM.render(<App />, document.getElementById("root"));
