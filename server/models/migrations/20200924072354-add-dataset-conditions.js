b40dcca505f6e07b7649f4e417380c7d6301b224
Razvan Ilin <razvanilin@gmail.com>
2020-09-24 08:43:54 +0100

    :construction: updated the dataset model with the conditions field
    also removed the axis type field that was not used

---
+++ server/models/migrations/20200924072354-add-dataset-conditions.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.addColumn("Dataset", "conditions", {
+          type: Sequelize.TEXT,
+          set(val) {
+            return this.setDataValue("conditions", JSON.stringify(val));
+          },
+          get() {
+            try {
+              return JSON.parse(this.getDataValue("conditions"));
+            } catch (e) {
+              return this.getDataValue("conditions");
+            }
+          }
+        }, { transaction: t }),
+        queryInterface.removeColumn("Dataset", "xAxisType", { transaction: t }),
+        queryInterface.removeColumn("Dataset", "yAxisType", { transaction: t }),
+      ])
+        .catch(() => Promise.resolve("done"));
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.removeColumn("Dataset", "conditions", { transaction: t }),
+        queryInterface.addColumn("Dataset", "xAxisType", {
+          type: Sequelize.STRING,
+        }, { transaction: t }),
+        queryInterface.addColumn("Dataset", "yAxisType", {
+          type: Sequelize.STRING,
+        }, { transaction: t }),
+      ])
+        .catch(() => Promise.resolve("done"));
+    });
+  }
+};
