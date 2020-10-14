9ceab1cfcbcb31b240681f502e53506013c1dc4b
Devin <devin@getcraft.com>
2020-10-14 19:01:14 +0700

    :hammer: refactor component FeedbackForm into functional component

--- client/src/components/FeedbackForm.js
+++ client/src/components/FeedbackForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import {
@@ -6,70 +6,61 @@ import {
 } from "semantic-ui-react";
 import { sendFeedback } from "../actions/user";
 
-class FeedbackForm extends Component {
-  constructor(props) {
-    super(props);
+function FeedbackForm(props) {
+  const [success, setSuccess] = useState(false);
+  const [submitError, setSubmitError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [feedback, setFeedback] = useState("");
+  const [name, setName] = useState("");
 
-    this.state = {
-      loading: false,
-      name: "",
-      feedback: "",
-      submitError: false,
-      success: false,
-    };
-  }
+  const { sendFeedback, user } = props
 
-  _onSendFeedback() {
-    const { sendFeedback, user } = this.props;
-    const { name, feedback } = this.state;
-
-    this.setState({ loading: true, success: false, submitError: false });
+  const _onSendFeedback = () => {
+    setLoading(true);
+    setSuccess(false);
+    setSubmitError(false);
     sendFeedback({
       name,
       feedback,
       email: user.email,
     })
       .then(() => {
-        this.setState({ loading: false, success: true });
+        setLoading(false);
+        setSuccess(true);
       })
       .catch(() => {
-        this.setState({ loading: false, submitError: true });
+        setLoading(false);
+        setSubmitError(true);
       });
-  }
+  };
 
-  render() {
-    const {
-      success, submitError, feedback, loading,
-    } = this.state;
-
-    return (
-      <div style={styles.container}>
-        <Header attached="top" as="h2">Feedback & Suggestions</Header>
-        <Segment raised attached>
-          <Header content="We would appreciate any feedback you may have" />
-          <Form>
-            <Form.Input
-              onChange={(e, data) => this.setState({ name: data.value })}
-              name="name"
-              label="Your name"
-              placeholder="You can leave it anonymous" />
-            <Form.TextArea
-              onChange={(e, data) => this.setState({ feedback: data.value })}
-              name="feedback"
-              label="Your Comments"
-              placeholder="Tell us about your exprience with our product" />
-          </Form>
-          <Divider hidden />
-          {success
-              && <Message positive content="We received your feedback and will work on it! Thank you." />}
-          {submitError
-              && <Message negative content="Something went wront, please try again or email us directly on info@depomo.com" />}
-          <Button loading={loading} onClick={() => this._onSendFeedback()} disabled={!feedback} floated="right" primary>Send</Button>
-          <Divider hidden section />
-        </Segment>
-      </div>
-    );
-  }
+  return (
+    <div style={styles.container}>
+      <Header attached="top" as="h2">Feedback & Suggestions</Header>
+      <Segment raised attached>
+        <Header content="We would appreciate any feedback you may have" />
+        <Form>
+          <Form.Input
+            onChange={(e, data) => setName(data.value)}
+            name="name"
+            label="Your name"
+            placeholder="You can leave it anonymous" />
+          <Form.TextArea
+            onChange={(e, data) => setFeedback(data.value)}
+            name="feedback"
+            label="Your Comments"
+            placeholder="Tell us about your exprience with our product" />
+        </Form>
+        <Divider hidden />
+        {success
+            && <Message positive content="We received your feedback and will work on it! Thank you." />}
+        {submitError
+            && <Message negative content="Something went wront, please try again or email us directly on info@depomo.com" />}
+        <Button loading={loading} onClick={() => _onSendFeedback()} disabled={!feedback} floated="right" primary>Send</Button>
+        <Divider hidden section />
+      </Segment>
+    </div>
+  );
 }
 
 const styles = {
