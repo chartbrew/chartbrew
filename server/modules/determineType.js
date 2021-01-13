3b0250f3c35e224bd3cd7ae6af6e547490b0dbf1
Razvan Ilin <razvanilin@gmail.com>
2021-01-13 13:45:57 +0000

    :construction: more work on better identifying dates

--- server/modules/determineType.js
+++ server/modules/determineType.js
@@ -17,8 +17,10 @@ function determineType(data) {
   if (typeof data !== "object" && !(data instanceof Array) && typeof data === "string") {
     dataType = "string";
   }
-  if ((moment(`${data})`).isValid() && ((typeof data === "number" && data.toString().length === 10) || typeof data !== "number"))
-    || (data && `${data}`.length === 10 && moment(data, "X").isValid())) {
+  if (data
+    && ((!Number.isNaN(new Date(data).getTime()) && `${data}`.length > 9)
+    || (moment(`${data})`).isValid() && ((typeof data === "number" && data.toString().length === 10) || typeof data !== "number"))
+    || (data && `${data}`.length === 10 && moment(data, "X").isValid()))) {
     dataType = "date";
   }
 
