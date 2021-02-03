afcdcdd50667b30daf29a69c8abd9675ae4d521b
Razvan Ilin <razvanilin@gmail.com>
2021-02-03 09:01:45 +0800

    :bug: fixed issue with the new project modal not appearing at the beginning

--- client/src/containers/UserDashboard.js
+++ client/src/containers/UserDashboard.js
@@ -57,11 +57,14 @@ function UserDashboard(props) {
       let teamOwned;
       teams.map((team) => {
         if (team.TeamRoles && _.find(team.TeamRoles, { user_id: user.data.id, role: "owner" })) {
+          console.log("team", team);
           teamOwned = team;
+
+          if (teamOwned.Projects && teamOwned.Projects.length === 0) {
+            shouldOpenNewProject = true;
+          }
         }
-        if (teams.Projects && teams.Projects.length > 0) {
-          shouldOpenNewProject = true;
-        }
+
         return team;
       });
 
