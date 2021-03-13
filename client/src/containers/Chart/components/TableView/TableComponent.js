86f0ac30c71bc21665f14abd87432a7e6d08d982
Razvan Ilin <razvanilin@gmail.com>
2021-03-13 15:26:00 +0800

    :construction: added pagination support

--- client/src/containers/Chart/components/TableView/TableComponent.js
+++ client/src/containers/Chart/components/TableView/TableComponent.js
@@ -1,9 +1,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
 import React from "react";
-import { useTable } from "react-table";
+import { usePagination, useTable } from "react-table";
 import PropTypes from "prop-types";
-import { Table } from "semantic-ui-react";
+import {
+  Button, Dropdown, Input, Table
+} from "semantic-ui-react";
+
+const paginationOptions = [5, 10, 20, 30, 40, 50].map((pageSize) => ({
+  key: pageSize,
+  value: pageSize,
+  text: `Show ${pageSize}`,
+}));
 
 function TableComponent(props) {
   const { columns, data, height } = props;
@@ -12,12 +20,23 @@ function TableComponent(props) {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    page,
     prepareRow,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
   } = useTable({
     columns,
     data,
-  });
+    initialState: { pageIndex: 0 }
+  },
+  usePagination);
 
   return (
     <div style={styles.mainBody(height)}>
@@ -32,7 +51,7 @@ function TableComponent(props) {
           ))}
         </Table.Header>
         <Table.Body {...getTableBodyProps()}>
-          {rows.map((row) => {
+          {page.map((row) => {
             prepareRow(row);
             return (
               <Table.Row {...row.getRowProps()}>
@@ -43,6 +62,48 @@ function TableComponent(props) {
             );
           })}
         </Table.Body>
+        <Table.Footer className="pagination">
+          <Table.Row>
+            <Table.HeaderCell colSpan={columns.length}>
+              <Button size="small" icon="angle double left" onClick={() => gotoPage(0)} disabled={!canPreviousPage} />
+              <Button size="small" icon="angle left" onClick={() => previousPage()} disabled={!canPreviousPage} />
+              <Button size="small" icon="angle right" onClick={() => nextPage()} disabled={!canNextPage} />
+              <Button size="small" icon="angle double right" onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage} />
+              <span>
+                {"Page "}
+                <strong>
+                  {pageIndex + 1}
+                  {" of "}
+                  {pageOptions.length}
+                </strong>
+              </span>
+              <span>
+                {" | Go to page: "}
+                <Input
+                  type="number"
+                  size="small"
+                  defaultValue={pageIndex + 1}
+                  onChange={e => {
+                    const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                    gotoPage(page);
+                  }}
+                  style={{ width: 100 }}
+                />
+              </span>
+              {" "}
+              <Dropdown
+                value={pageSize}
+                onChange={(e, data) => {
+                  setPageSize(Number(data.value));
+                }}
+                options={paginationOptions}
+                selection
+                compact
+                size="small"
+              />
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
       </Table>
     </div>
   );
