c53c49b5932482181d600071028edc111f1aa795
Razvan Ilin <razvanilin@gmail.com>
2021-07-20 11:41:30 +0300

    :whale: removed the migration command from the docker entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ export REACT_APP_API_HOST=${REACT_APP_API_HOST}
 export REACT_APP_CLIENT_HOST=${REACT_APP_CLIENT_HOST}
 export REACT_APP_ONE_ACCOUNT_EXTERNAL_ID=${REACT_APP_ONE_ACCOUNT_EXTERNAL_ID}
 
-cd server && NODE_ENV=production npm run db:migrate
+cd server
 NODE_ENV=production nohup node index.js &
 
 echo -e "\nBuilding the UI. This might take a couple of minutes...\n"
