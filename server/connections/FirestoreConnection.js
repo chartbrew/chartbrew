16e93facd88ba7797d2998b23ac51d405599917e
Razvan Ilin <razvanilin@gmail.com>
2021-09-21 18:15:41 +0300

    :construction: populated the main response with the subcollections

--- server/connections/FirestoreConnection.js
+++ server/connections/FirestoreConnection.js
@@ -24,9 +24,6 @@ class FirestoreConnection {
     }
 
     this.db = this.admin.firestore();
-
-    // seed data
-    // firestoreSeed(this.db, "client");
   }
 
   async get(dataRequest) {
@@ -83,15 +80,48 @@ class FirestoreConnection {
       });
     }
     const formattedDocs = [];
+
     const docs = await docsRef.get();
-    docs.forEach((doc) => {
-      formattedDocs.push(doc.data());
+    const subCollectionsPromises = [];
+    docs.forEach(async (doc) => {
+      subCollectionsPromises.push(doc.ref.listCollections());
+      formattedDocs.push({ ...doc.data(), _id: doc.id });
+    });
+
+    const subDataPromises = [];
+    const subCollections = await Promise.all(subCollectionsPromises);
+    subCollections.forEach((collections) => {
+      collections.forEach((sub) => {
+        subDataPromises.push(
+          sub.get().then((snapshot) => {
+            return { snapshot, parent: sub.parent.id, collection: sub.id };
+          })
+        );
+      });
+    });
+
+    const subData = [];
+    const subResults = await Promise.all(subDataPromises);
+    subResults.forEach((sub) => {
+      sub.snapshot.docs.forEach((d) => {
+        subData.push({ ...d.data(), _parent: sub.parent, _collection: sub.collection });
+      });
     });
 
     const nestedCheckedDocs = [];
     let index = -1;
-    formattedDocs.forEach((doc) => {
-      nestedCheckedDocs.push(doc);
+    formattedDocs.forEach((data) => {
+      const doc = data;
+
+      // populate sub collections
+      subData.forEach((sub) => {
+        if (doc._id === sub._parent) {
+          if (!doc[sub._collection]) doc[sub._collection] = [];
+          doc[sub._collection].push(sub);
+        }
+      });
+
+      nestedCheckedDocs.push({ ...doc });
       index++;
       Object.keys(doc).forEach((key) => {
         if (determineType(doc[key]) === "array") {
