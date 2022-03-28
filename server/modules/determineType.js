18d2a755ae10d62dee3aab84c02779950f2daaac
Razvan Ilin <razvanilin@gmail.com>
2022-03-28 14:43:19 +0700

    :construction: added a date check for timestamps in a string format

--- server/modules/determineType.js
+++ server/modules/determineType.js
@@ -22,6 +22,7 @@ function determineType(data) {
     if (data
       && ((!Number.isNaN(new Date(data).getTime()) && `${data}`.length > 9 && `${data}`.replace(/\D/g, "").length > 3)
       || (moment(`${data})`).isValid() && ((typeof data === "number" && data.toString().length === 10) || typeof data !== "number"))
+      || (moment(`${data})`, "X").isValid() && (typeof data === "string" && data.length === 10))
       || (data && `${data}`.length === 10 && `${data}`[0] === "1" && moment(data, "X").isValid() && typeof data === "number"))) {
       dataType = "date";
     }
