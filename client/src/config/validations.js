cd2e6b1388b2dca431585140bac64eaa65c72271
E <expatiating@users.noreply.github.com>
2022-10-07 18:29:28 -0700

    :adhesive_bandage: updated email validation regex

--- client/src/config/validations.js
+++ client/src/config/validations.js
@@ -1,5 +1,5 @@
 export const required = value => (value ? "" : "This field is required");
 
-export const email = value => (!value || (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) ? "Invalid email address" : "");
+export const email = value => (!value || (value && !/^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/i.test(value)) ? "Invalid email address" : "");
 
 export const password = value => (!value || (value && !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$/i.test(value)) ? "Minimum 8 characters, at least 1 letter and 1 number" : "");
