9e40e38ec54b1accf1d2b21f7f950eb85f62d70f
Razvan Ilin <razvanilin@gmail.com>
2022-10-10 14:11:21 +0800

    :construction: relaxed the email validation to allow more all types of emails

--- client/src/config/validations.js
+++ client/src/config/validations.js
@@ -1,5 +1,5 @@
 export const required = value => (value ? "" : "This field is required");
 
-export const email = value => (!value || (value && !/^[a-zA-Z0-9.!#$%&’*+=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/i.test(value)) ? "Invalid email address" : "");
+export const email = value => (!value || (value && !/^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$/i.test(value)) ? "Invalid email address" : "");
 
 export const password = value => (!value || (value && !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$/i.test(value)) ? "Minimum 8 characters, at least 1 letter and 1 number" : "");
