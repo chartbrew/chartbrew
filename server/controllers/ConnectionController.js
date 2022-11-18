b81f18ee0004eb59cc04d0bc1d591afaa42111dc
Razvan Ilin <razvanilin@gmail.com>
2022-11-18 15:31:52 +0700

    :construction: added backend caching capabilities for all types of data requests

--- server/controllers/ConnectionController.js
+++ server/controllers/ConnectionController.js
@@ -806,11 +806,44 @@ class ConnectionController {
       });
   }
 
-  async runGoogleAnalytics(connection, dataRequest) {
+  async runGoogleAnalytics(connection, dataRequest, getCache) {
+    if (getCache) {
+      // check if there is a cache available and valid
+      try {
+        const drCache = await drCacheController.findLast(dataRequest.id);
+        const cachedDataRequest = drCache.dataRequest;
+        cachedDataRequest.updatedAt = "";
+        cachedDataRequest.createdAt = "";
+
+        const liveDataRequest = dataRequest.toJSON();
+        liveDataRequest.updatedAt = "";
+        liveDataRequest.createdAt = "";
+
+        if (_.isEqual(cachedDataRequest, liveDataRequest)) {
+          return drCache.responseData;
+        }
+      } catch (e) {
+        //
+      }
+    }
+
     if (!connection.oauth_id) return Promise.reject({ error: "No oauth token" });
 
     const oauth = await oauthController.findById(connection.oauth_id);
-    return googleConnector.getAnalytics(oauth, dataRequest);
+    return googleConnector.getAnalytics(oauth, dataRequest)
+      .then((responseData) => {
+        // cache the data for later use
+        const dataToCache = {
+          dataRequest,
+          responseData,
+        };
+        drCacheController.create(dataRequest.id, dataToCache);
+
+        return responseData;
+      })
+      .catch((err) => {
+        return new Promise((resolve, reject) => reject(err));
+      });
   }
 
   async testGoogleAnalytics(connection) {
