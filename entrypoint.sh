c42b804ebf53c5a1f9e53ed76def6ec5a60123f0
Razvan Ilin <razvanilin@gmail.com>
2023-01-11 11:15:10 +0700

    :whale: few more changes to make sure the entrypoint script keeps both apps alive

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,14 @@ export REACT_APP_API_HOST=${REACT_APP_API_HOST}
 export REACT_APP_CLIENT_HOST=${REACT_APP_CLIENT_HOST}
 export REACT_APP_ONE_ACCOUNT_EXTERNAL_ID=${REACT_APP_ONE_ACCOUNT_EXTERNAL_ID}
 
-cd client
-mkdir -p dist
-npx serve -s -p 4018 dist
-npm run build && cp -rf build/* dist/ &
-
-cd ../server
+cd server
 NODE_ENV=production nohup node index.js &
 
+cd ../client
+mkdir -p dist
+
+# Build the UI in the background
+nohup sh -c 'echo "The UI is rebuilding. Please wait..." && npm run build && echo "UI built successfully!" && cp -rf build/* dist/' &
+
+# Serve the UI
+npx serve -s -p 4018 dist
