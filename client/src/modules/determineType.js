6203fe41be971017007a6eec23fd5993914a4ea7
Razvan Ilin <razvanilin@gmail.com>
2023-07-20 17:41:55 +0700

    :rocket: updated the date check statements

--- client/src/modules/determineType.js
+++ client/src/modules/determineType.js
@@ -1,11 +1,15 @@
 import moment from "moment";
 
-export default function determineType(data) {
-  // regex to check if the string is made only of numbers
-  const checkNumbersOnly = /^\d+$/;
-  // regex to check if numbers only and if length is 10 or 13
-  const checkNumbersOnlyAndLength = /^\d{10,13}$/;
+// regex to check if the string is made only of numbers
+const checkNumbersOnly = /^\d+$/;
+// regex to check if numbers only and if length is 10 or 13
+const checkNumbersOnlyAndLength = /^\d{10,13}$/;
+// regex to check if a string is the name of the months no matter the case
+const checkMonthNameShort = /^(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)$/i;
+// regex to check if a string is the name of the months no matter the case
+const checkMonthNameLong = /^(january|february|march|april|may|june|july|august|september|october|november|december)$/i;
 
+export default function determineType(data) {
   let dataType;
   if (data !== null && typeof data === "object" && data instanceof Array) {
     dataType = "array";
@@ -26,7 +30,7 @@ export default function determineType(data) {
   try {
     if (data
       && ((!Number.isNaN(new Date(data).getTime()) && `${data}`.length > 9 && `${data}`.replace(/\D/g, "").length > 3 && `${data}`.replace(/\D/g, "").length < 14 && (`${data}`[0] === "1" || `${data}`[0] === "2"))
-      || (moment(`${data}`).isValid() && !checkNumbersOnlyAndLength.test(data) && ((typeof data === "number" && data.toString().length === 10) || typeof data !== "number"))
+      || (moment(`${data}`).isValid() && !checkNumbersOnlyAndLength.test(data) && ((typeof data === "number" && data.toString().length === 10) || (typeof data !== "number" && (checkMonthNameLong(data) || checkMonthNameShort(data)))))
       || (moment(`${data}`, "X").isValid() && (typeof data === "string" && data.length === 10) && checkNumbersOnlyAndLength.test(data))
       || (data && `${data}`.length === 10 && `${data}`[0] === "1" && moment(data, "X").isValid() && typeof data === "number"))) {
       dataType = "date";
