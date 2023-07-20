021630214e499f68a6df06928ebae1c5300b42d7
Razvan Ilin <razvanilin@gmail.com>
2023-07-20 18:36:10 +0700

    :bug: fixed date check

--- server/modules/determineType.js
+++ server/modules/determineType.js
@@ -4,10 +4,6 @@ const moment = require("moment");
 const checkNumbersOnly = /^\d+$/;
 // regex to check if numbers only and if length is 10 or 13
 const checkNumbersOnlyAndLength = /^\d{10,13}$/;
-// regex to check if a string is the name of the months no matter the case
-const checkMonthNameShort = /^(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)$/i;
-// regex to check if a string is the name of the months no matter the case
-const checkMonthNameLong = /^(january|february|march|april|may|june|july|august|september|october|november|december)$/i;
 
 function determineType(data) {
   let dataType;
@@ -30,7 +26,7 @@ function determineType(data) {
   try {
     if (data
       && ((!Number.isNaN(new Date(data).getTime()) && `${data}`.length > 9 && `${data}`.replace(/\D/g, "").length > 3 && `${data}`.replace(/\D/g, "").length < 14 && (`${data}`[0] === "1" || `${data}`[0] === "2"))
-      || (moment(`${data}`).isValid() && !checkNumbersOnlyAndLength.test(data) && ((typeof data === "number" && data.toString().length === 10) || (typeof data !== "number" && (checkMonthNameLong(data) || checkMonthNameShort(data)))))
+      || (moment(`${data}`).isValid() && !checkNumbersOnlyAndLength.test(data) && ((typeof data === "number" && data.toString().length === 10) || (typeof data !== "number" && !checkNumbersOnly.test(data))))
       || (moment(`${data}`, "X").isValid() && (typeof data === "string" && data.length === 10) && checkNumbersOnlyAndLength.test(data))
       || (data && `${data}`.length === 10 && `${data}`[0] === "1" && moment(data, "X").isValid() && typeof data === "number"))) {
       dataType = "date";
