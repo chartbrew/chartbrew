6d1ba289a2ac3db899254e119e2b482b7f6a4ebb
Razvan Ilin <razvanilin@gmail.com>
2023-07-24 14:45:28 +0700

    :bug: fixed issue with the custom templates where the charts would still use the old connection instead of the newly created one

--- server/templates/custom/builder.js
+++ server/templates/custom/builder.js
@@ -31,135 +31,185 @@ module.exports = async (projectId, { template_id, charts, connections }) => {
     });
   }
 
-  const createAndRecordConnection = async (connection) => {
-    if (_.indexOf(avoidConnectionCreationList, connection.id) > -1
-      && !connections[connection.id]?.createNew
-    ) {
-      return new Promise((resolve) => resolve(connection));
+  const createConnections = async (conns, index = 0, connectionResults = []) => {
+    if (index >= conns.length) {
+      return createdConnections;
     }
 
-    const newConnection = _.clone(connection);
+    const currConnection = conns[index];
+    const newConnection = _.clone(currConnection);
+
+    if (currConnection
+      && _.indexOf(avoidConnectionCreationList, currConnection.id) > -1
+      && !connections[currConnection.id]?.createNew
+    ) {
+      connectionResults.push(newConnection);
+      return createConnections(conns, index + 1, connectionResults);
+    }
+
     newConnection.project_id = projectId;
     delete newConnection.id;
 
-    avoidConnectionCreationList.push(connection.id);
+    avoidConnectionCreationList.push(currConnection.id);
 
     const createdConnection = await db.Connection.create(newConnection);
-    createdConnections[connection.id] = createdConnection.id;
+    createdConnections[currConnection.id] = createdConnection.id;
 
-    return createdConnection;
+    connectionResults.push(createdConnection);
+
+    return createConnections(conns, index + 1, connectionResults);
   };
 
-  const createConnections = async (connections) => {
-    const promises = connections.map(async (c) => {
-      return createAndRecordConnection(c);
-    });
+  const prepareDataRequests = async (dataRequests, datasetId, index = 0, drResults = []) => {
+    if (index >= dataRequests.length) {
+      return drResults;
+    }
 
-    return Promise.all(promises);
-  };
-
-  const prepareDataRequests = async (dataRequests, datasetId) => {
-    const newDataRequests = dataRequests;
+    const newDr = { ...dataRequests[index], dataset_id: datasetId };
 
     // first create the connections
     const connectionsToCreate = model.Connections.filter((c) => {
-      return newDataRequests.find((dr) => dr.Connection === c.id);
+      return newDr.Connection === c.id;
     });
 
-    const createdConnections = await createConnections(connectionsToCreate);
+    let createdConnectionsTemp = createdConnections;
+    try {
+      createdConnectionsTemp = await createConnections(connectionsToCreate);
+    } catch (e) {
+      // do nothing
+    }
 
-    const drsToCreate = [];
-    dataRequests.forEach((dr) => {
-      const newDr = { ...dr, dataset_id: datasetId };
-      if (createdConnections[dr.Connection]) {
-        newDr.connection_id = createdConnections[dr.Connection];
-      } else {
-        newDr.connection_id = dr.Connection;
-      }
+    if (createdConnectionsTemp[newDr.Connection]) {
+      newDr.connection_id = createdConnectionsTemp[newDr.Connection];
+    } else {
+      newDr.connection_id = newDr.Connection;
+    }
 
-      drsToCreate.push(newDr);
-    });
+    drResults.push(newDr);
 
-    return drsToCreate;
+    return prepareDataRequests(dataRequests, datasetId, index + 1, drResults);
   };
 
-  const createDatasets = async (datasets) => {
-    const promises = datasets.map(async (d) => {
-      const dataset = d;
-      if (dataset.DataRequest) {
-        dataset.DataRequests = [dataset.DataRequest];
-      }
+  const createDataRequests = async (dataRequests, index = 0, drResults = []) => {
+    if (index >= dataRequests.length) {
+      return drResults;
+    }
 
-      return db.Dataset.create(dataset)
-        .then(async (createdDataset) => {
-          const drsToCreate = await prepareDataRequests(
-            dataset.DataRequests,
-            createdDataset.id
-          );
-
-          const drPromises = [];
-          drsToCreate.forEach((dr) => {
-            drPromises.push(
-              db.DataRequest.create(dr),
-            );
-          });
-          Promise.all(drPromises);
-          return createdDataset;
-        });
-    });
-
-    return Promise.all(promises);
+    const dr = dataRequests[index];
+    try {
+      const newDr = await db.DataRequest.create(dr);
+      drResults.push(newDr);
+      return createDataRequests(dataRequests, index + 1, drResults);
+    } catch (e) {
+      return createDataRequests(dataRequests, index + 1, drResults);
+    }
   };
 
-  // now go through all the charts
-  const chartPromises = model.Charts.map(async (chart) => {
-    const newChart = chart;
-    newChart.project_id = projectId;
-    const newPromise = await db.Chart.create(newChart)
-      .then(async (createdChart) => {
-        // const datasetsToCreate = await preparedDatasets(
-        //   chart.Datasets,
-        //   createdChart.id
-        // );
-        const datasetsToCreate = chart.Datasets.map((d) => {
-          return {
-            ...d,
-            chart_id: createdChart.id,
-          };
-        });
+  const createDatasets = async (datasets, index = 0, datasetsResult = []) => {
+    if (index >= datasets.length) {
+      return datasetsResult;
+    }
 
-        const createdDatasets = await createDatasets(datasetsToCreate);
+    const dataset = datasets[index];
+    if (dataset.DataRequest) {
+      dataset.DataRequests = [dataset.DataRequest];
+    }
 
+    return db.Dataset.create(dataset)
+      .then(async (createdDataset) => {
+        const drsToCreate = await prepareDataRequests(
+          dataset.DataRequests,
+          createdDataset.id
+        );
+
+        await createDataRequests(drsToCreate);
+
+        datasetsResult.push(createdDataset);
+        return createDatasets(datasets, index + 1, datasetsResult);
+      })
+      .catch(() => {
+        return createDatasets(datasets, index + 1, datasetsResult);
+      });
+  };
+
+  const createCharts = async (modelCharts, index = 0, chartsResult = []) => {
+    if (index >= modelCharts.length) {
+      return chartsResult;
+    }
+
+    const chart = modelCharts[index];
+    chart.project_id = projectId;
+
+    const newChart = await db.Chart.create(chart);
+
+    try {
+      const datasetsToCreate = chart.Datasets.map((d) => {
         return {
-          chart: createdChart,
-          datasets: createdDatasets,
+          ...d,
+          chart_id: newChart.id,
         };
       });
 
-    return newPromise;
+      const createdDatasets = await createDatasets(datasetsToCreate);
+
+      chartsResult.push({
+        chart: newChart,
+        datasets: createdDatasets,
+      });
+    } catch (e) {
+      // do nothing
+    }
+
+    return createCharts(modelCharts, index + 1, chartsResult);
+  };
+
+  const createdCharts = await createCharts(model.Charts);
+
+  // now go through all the charts
+  // const chartPromises = model.Charts.map(async (chart) => {
+  //   const newChart = chart;
+  //   newChart.project_id = projectId;
+  //   const newPromise = await db.Chart.create(newChart)
+  //     .then(async (createdChart) => {
+  //       // const datasetsToCreate = await preparedDatasets(
+  //       //   chart.Datasets,
+  //       //   createdChart.id
+  //       // );
+  //       const datasetsToCreate = chart.Datasets.map((d) => {
+  //         return {
+  //           ...d,
+  //           chart_id: createdChart.id,
+  //         };
+  //       });
+
+  //       const createdDatasets = await createDatasets(datasetsToCreate);
+
+  //       return {
+  //         chart: createdChart,
+  //         datasets: createdDatasets,
+  //       };
+  //     });
+
+  //   return newPromise;
+  // });
+
+  const gResult = createdCharts;
+
+  const updatePromises = [];
+  createdCharts.forEach((chart) => {
+    chart.datasets.forEach((dataset) => {
+      if (createdConnections[dataset.connection_id]) {
+        updatePromises.push(
+          db.Dataset.update(
+            { connection_id: createdConnections[dataset.connection_id] },
+            { where: { id: dataset.id } }
+          )
+        );
+      }
+    });
   });
 
-  let gResult;
-  return Promise.all(chartPromises)
-    .then((charts) => {
-      gResult = charts;
-
-      const updatePromises = [];
-      charts.forEach((chart) => {
-        chart.datasets.forEach((dataset) => {
-          if (createdConnections[dataset.connection_id]) {
-            updatePromises.push(
-              db.Dataset.update(
-                { connection_id: createdConnections[dataset.connection_id] },
-                { where: { id: dataset.id } }
-              )
-            );
-          }
-        });
-      });
-
-      return Promise.all(updatePromises);
-    })
+  return Promise.all(updatePromises)
     .then(() => {
       return gResult;
     })
