307898b7e6ee3eba4616960a8d005bce0ccb7125
Razvan Ilin <razvanilin@gmail.com>
2023-12-25 17:39:09 +0700

    :wrench: changed env vars in settings to VITE_

--- client/src/config/settings.template.js
+++ client/src/config/settings.template.js
@@ -1,12 +1,12 @@
 export const API_HOST = import.meta.env.PROD
-  ? import.meta.env.REACT_APP_API_HOST
-  : (import.meta.env.REACT_APP_API_HOST_DEV || "http://localhost:4019");
+  ? import.meta.env.VITE_APP_API_HOST
+  : (import.meta.env.VITE_APP_API_HOST_DEV || "http://localhost:4019");
 
 export const SITE_HOST = import.meta.env.PROD
-  ? import.meta.env.REACT_APP_CLIENT_HOST
-  : (import.meta.env.REACT_APP_CLIENT_HOST_DEV || "http://localhost:4018");
+  ? import.meta.env.VITE_APP_CLIENT_HOST
+  : (import.meta.env.VITE_APP_CLIENT_HOST_DEV || "http://localhost:4018");
 
 export const DOCUMENTATION_HOST = import.meta.env.PROD ? "https://docs.chartbrew.com" : "http://localhost:8080";
 
-export const APP_VERSION = import.meta.env.REACT_APP_VERSION;
-export const ONE_ACCOUNT_ENABLED = !!import.meta.env.REACT_APP_ONE_ACCOUNT_EXTERNAL_ID;
+export const APP_VERSION = import.meta.env.VITE_APP_VERSION;
+export const ONE_ACCOUNT_ENABLED = !!import.meta.env.VITE_APP_ONE_ACCOUNT_EXTERNAL_ID;
