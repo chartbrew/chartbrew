7a46be888d954b9d4c8e5e7617849fb6bc611401
Razvan Ilin <razvanilin@gmail.com>
2023-12-25 15:07:20 +0700

    :wrench: changed vite config to fetch env vars from the project's root folder

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 4018,
-  },
-})
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, `${process.cwd()}/..`) }; // eslint-disable-line
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 4018,
+    },
+  });
+};
