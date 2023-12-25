889532466db0802310484133948fca5d9dbb1db3
Razvan Ilin <razvanilin@gmail.com>
2023-12-25 18:13:21 +0700

    :whale: updated the docker entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # all env vars used in the client app need to be set here as well
-export REACT_APP_API_HOST=${REACT_APP_API_HOST}
-export REACT_APP_CLIENT_HOST=${REACT_APP_CLIENT_HOST}
-export REACT_APP_ONE_ACCOUNT_EXTERNAL_ID=${REACT_APP_ONE_ACCOUNT_EXTERNAL_ID}
+export VITE_APP_API_HOST=${VITE_APP_API_HOST}
+export VITE_APP_CLIENT_HOST=${VITE_APP_CLIENT_HOST}
+export VITE_APP_CLIENT_PORT=${VITE_APP_CLIENT_PORT}
+export VITE_APP_ONE_ACCOUNT_EXTERNAL_ID=${VITE_APP_ONE_ACCOUNT_EXTERNAL_ID}
 
 cd server
 NODE_ENV=production nohup node index.js &
 
 cd ../client
-mkdir -p dist
+# mkdir -p dist
 
 # Build the UI in the background
-nohup sh -c 'echo "The UI is rebuilding. Please wait..." && npm run build && echo "UI built successfully!" && cp -rf build/* dist/' &
+# nohup sh -c 'echo "The UI is rebuilding. Please wait..." && npm run build && echo "UI built successfully!" && cp -rf build/* dist/' &
+nohup sh -c 'echo "The UI is rebuilding. Please wait..." && npm run build && echo "UI built successfully!"' &
 
 # Serve the UI
-npx serve -s -p 4018 dist
+npm run preview
