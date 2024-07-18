d204e8ee230655439b97a98f46c24a8782e4e41a
Razvan Ilin <razvanilin@gmail.com>
2024-07-18 17:30:58 +0700

    :children_crossing: updated the formulat tips with a rounding example and a list of all available formulas

--- client/src/components/FormulaTips.jsx
+++ client/src/components/FormulaTips.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Code, Spacer } from "@nextui-org/react";
-import { LuChevronRight } from "react-icons/lu";
+import { LuChevronRight, LuExternalLink } from "react-icons/lu";
 
 import Text from "./Text";
 import Row from "./Row";
@@ -52,6 +52,26 @@ function FormulaTips() {
           <Code>{"{val / 100} USD"}</Code>
           {" => 123.45 USD"}
         </Text>
+      </Row>
+      <Spacer y={1} />
+      <Row align="center">
+        <LuChevronRight />
+        <Spacer x={0.5} />
+        <Text>
+          <Code>{"{ROUND(val / 100, 0)}"}</Code>
+          {" => 123"}
+        </Text>
+      </Row>
+      <Spacer y={2} />
+      <Row align="center">
+        <div>
+          {"For a full list of formulas, "}
+          <a href="https://github.com/handsontable/formula-parser/blob/develop/src/supported-formulas.js">
+            {"visit this page"}
+          </a>
+        </div>
+        <Spacer x={0.5} />
+        <LuExternalLink size={16} />
       </Row>
     </div>
   );
